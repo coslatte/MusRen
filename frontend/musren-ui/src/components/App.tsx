8815eb5b064f411052cc72de24344d834dbf15d5
coslatte <gabrielpazruiz02@gmail.com>
2025-10-26 22:20:22 -0400

    Add base template for developing GUI

---
+++ frontend/musren-ui/src/components/App.tsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div className="text-3xl font-bold underline">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem provident
+      pariatur facilis corrupti ad, laborum sint amet temporibus illo voluptas
+      nisi delectus officiis ut, harum aperiam tempora, incidunt asperiores
+      eius!
+    </div>
+  );
+}
